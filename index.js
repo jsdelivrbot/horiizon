37952369d7445bc3599051231646869d8a3e293b
Marc-Andre <lauzier.ma@gmail.com>
2017-08-14 12:13:15 -0400

    index.js changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var app = express();
 
@@ -10,30 +9,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     response.send('Whats up? from horiizon World');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
-=======
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 8080));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-    response.send('Hi World');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
->>>>>>> fd9fe279fa95602077dee01bbe6f66a28c996443
